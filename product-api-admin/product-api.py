566848cb06b16806360f84327a65511e45bcc5cb
GeorgioZeilaa <georgio.antoni@hotmail.com>
2020-12-22 18:22:57 +0200

    Implementation of admin API and index webpage

--- product-api-admin/product-api.py
+++ product-api-admin/product-api.py
@@ -1,26 +1,29 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, render_template, request
 import mysql.connector
-
 app = Flask(__name__)
-api = Api(app)
 
-class Product(Resource):
-    def get(self):
-        config = {
+
+config = {
             'user':'root','password':'root',
             'host': 'db-mysql','port':'3306',
             'database':'products'
         }
-        
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        Pname = details['Pname']
+        Quantity = details['Quantity']
+        Price = details['Price']
+
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM items_on_sale")
-        results = [{"item":name, 'qty':qty,'price':price} for (name,qty,price) in cursor]
-        return {'products':results}
-        
-api.add_resource(Product, '/')
+        cursor.execute("INSERT INTO items_on_sale(name, QTY, Price) VALUES (%s, %s, %s)", (Pname, Quantity, Price))
+        mysql.connection.commit()
+        cursor.close()
+        return 'success'
+    return render_template('http://website-admin')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
-    
