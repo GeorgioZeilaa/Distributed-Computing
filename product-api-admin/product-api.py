b2b1e533af310048a51309ef225f07c39e235688
GeorgioZeilaa <georgio.antoni@hotmail.com>
2020-12-23 13:56:22 +0200

    Implementation of admin API and index webpage

--- product-api-admin/product-api.py
+++ product-api-admin/product-api.py
@@ -1,29 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, redirect
+from flask_restful import Resource, Api
 import mysql.connector
+
 app = Flask(__name__)
+api = Api(app)
 
+@app.route('/set', methods=['POST'])
+def set(self):
+    config = {
+        'user':'root','password':'root',
+        'host': 'db-mysql','port':'3306',
+        'database':'products'
+    }
 
-config = {
-            'user':'root','password':'root',
-            'host': 'db-mysql','port':'3306',
-            'database':'products'
-        }
+    pname = request.form['Pname']
+    qty = request.form['Quantity']
+    price = request.form['Price']
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == "POST":
-        details = request.form
-        Pname = details['Pname']
-        Quantity = details['Quantity']
-        Price = details['Price']
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    sql = "INSERT INTO items_on_sale (name, qty, price) VALUES (%s, %s, %s)"
+    val = (pname, qty, price)
+    cursor.execute(sql,val)
+    return redirect('/')
 
-        connection = mysql.connector.connect(**config)
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO items_on_sale(name, QTY, Price) VALUES (%s, %s, %s)", (Pname, Quantity, Price))
-        mysql.connection.commit()
-        cursor.close()
-        return 'success'
-    return render_template('http://website-admin')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+if __name__=='__main__':
+    app.run(port=80,debug=True)
--- website1/index.php
+++ website1/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Currently on sale1:</h1>
+<h1>Currently on sale:</h1>
+<h2>Host: <!--#echo var="HOSTNAME" --></h2>
 <?php
     $json = file_get_contents('http://product-api-service');
     $temp = json_decode($json, true);
-
+    
     foreach($temp["products"] as $product)
     {
         echo "<p>";
